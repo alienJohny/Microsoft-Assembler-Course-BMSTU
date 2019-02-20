0a4ada566211874f58c1a3d9d144088b821c19c5
Artur Yumaev <art.yumaev@gmail.com>
2019-02-20 18:38:12 +0300

    ATTENTION! BAG IN TEST.ASM

--- TEST1_25.2.19/test.asm
+++ TEST1_25.2.19/test.asm
@@ -1,39 +1,42 @@
 SSEG SEGMENT PARA STACK 'STACK'
-	DB 64 DUP(0)
+	db 64 DUP(0)
 SSEG ENDS
 
 DSEG SEGMENT PARA 'DATA'
-	X DB 'A',0
+	X db 'A'
 DSEG ENDS
 
 CSEG SEGMENT PARA 'CODE'
 	ASSUME CS:CSEG, DS:DSEG, SS:SSEG
 
-; ENTRY POINT
-MAIN:
-	MOV AX, DSEG
-	MOV DS, AX
+; entry point
+main:
+	mov AX, DSEG
+	mov DS, AX
 	
-	; PRINTING CHARS FROM A TO D
-	MOV CX, 5
-	LOOP PRINT_ASCII		
+	; printing chars from 'A' to 'E'
+	mov CX, 5
+	jmp printASCII
 
-	JMP EXIT
+printASCII:
+	mov DL, X
+	call print_symbol_from_DL
+	inc X
+	loop printASCII
+	jmp ExitProcess
 
-PRINT_ASCII:
-	JMP PRINT_SYMBOL_FROM_DL
-	INC X
-	RET
-		
-; OUTPUT SYMBOLS
-PRINT_SYMBOL_FROM_DL:
-	MOV AH, 02H
-	INT 21H
-	RET
+print_symbol_from_DL PROC
+	; DOS Fn 02H: display output
+	; AH = 02H
+	; DL = The symbol displayed on the standard output
+	mov AH, 02h
+	int 21h
+	ret
+print_symbol_from_DL ENDP
 
-; EXIT APPLICATION
-EXIT:
-	MOV AH, 4CH
+; Exit application
+ExitProcess:
+	mov AH, 4CH
 	INT 21H
 	
 CSEG ENDS
