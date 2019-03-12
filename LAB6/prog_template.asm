5dcf93d6e9da6228de11ed52da785c3e38197458
Artur Yumaev <art.yumaev@gmail.com>
2019-03-12 22:37:23 +0300

    created lab 6

---
+++ LAB6/prog_template.asm
@@ -0,0 +1,23 @@
+sseg segment para stack 'stack'
+	db 64 dup(0)
+sseg ends
+
+dseg segment para 'data'
+	;Some data
+dseg ends
+
+cseg segment para 'code'
+	assume CS:cseg, DS:dseg, SS:sseg
+
+;Exit application
+ExitProcess:
+	mov AH, 4Ch
+	int 21h
+
+;Entry point
+main:
+	;Some code ...
+
+	jmp ExitProcess
+cseg ends
+end main
