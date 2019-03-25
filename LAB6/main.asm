a36cbe85fb8f1b69b138ccba20380146b767e470
Artur Yumaev <art.yumaev@gmail.com>
2019-03-25 10:36:48 +0300

    update

--- LAB6/main.asm
+++ LAB6/main.asm
@@ -1,101 +1,100 @@
-EXTRN	SCAN : NEAR
-EXTRN	F1 : NEAR	; unsigned	bin
-EXTRN	F2 : NEAR	; signed	bin
-EXTRN	F3 : NEAR	; unsigned	dec
-EXTRN	F4 : NEAR	; signed	dec
-EXTRN	F5 : NEAR	; unsigned	hex
-EXTRN	F6 : NEAR	; signed	hex
+extrn SCAN : near
+extrn F1 : near	; unsigned bin
+extrn F2 : near	; signed bin
+extrn F3 : near	; unsigned dec
+extrn F4 : near	; signed dec
+extrn F5 : near	; unsigned hex
+extrn F6 : near	; signed hex
 
-SSTACK	SEGMENT PARA STACK 'STACK'
-		DB		64 DUP('STACK___')
-SSTACK	ENDS
+sstack segment para stack 'stack'
+    db 64 dup(0)
+sstack	ends
 
-DSEG	SEGMENT PARA PUBLIC 'DATA'
-		F		DW	F1, F2, F3, F4, F5, F6
-		X		DW	5
+dseg segment para public 'DATA'
+    F DW F1, F2, F3, F4, F5, F6
+    X DW 5
 		
-		MENU	DB	'	MENU', 10, 13
-				DB	'0. Print menu', 10, 13
-				DB	'1. Input number', 10, 13
-				DB	'2. Num as unsigned bin', 10, 13
-				DB	'3. Num as signed bin', 10, 13
-				DB	'4. Num as unsigned dec', 10, 13
-				DB	'5. Num as signed dec', 10, 13
-				DB	'6. Num as unsigned hex', 10, 13
-				DB	'7. Num as signed hex', 10, 13
-				DB	'8. Exit', 10, 13, '$'
-		
-		ENT		DB	'> $'
-		NLINE	DB	10, 13, '$'
-DSEG	ENDS
+    MENU db '    MENU', 10, 13
+         db '0. Print menu', 10, 13
+         db '1. Input number', 10, 13
+         db '2. Num as unsigned bin', 10, 13
+         db '3. Num as signed bin', 10, 13
+         db '4. Num as unsigned dec', 10, 13
+         db '5. Num as signed dec', 10, 13
+         db '6. Num as unsigned hex', 10, 13
+         db '7. Num as signed hex', 10, 13
+         db '8. Exit', 10, 13, '$'
 
-CSEG	SEGMENT PARA PUBLIC 'CODE'
-		ASSUME CS:CSEG, DS:DSEG, SS:SSTACK
+    ENT db '> $'
+    NLINE db 10, 13, '$'
+dseg ends
+
+cseg segment para public 'CODE'
+    ASSUME CS:cseg, DS:dseg, SS:sstack
 BEGIN:
-		MOV  AX, DSEG
-		MOV  DS, AX
+    mov  AX, dseg
+    mov  DS, AX
 PMENU:
-		MOV  AH, 9
-		MOV  DX, OFFSET MENU
-		INT  21H
-		
+    mov  AH, 09h
+    mov  DX, offset MENU
+    int  21H
+
 PRINT_ENT:
-		MOV  AH, 9
-		MOV  DX, OFFSET ENT
-		INT  21H
+    mov  AH, 09h
+    mov  DX, offset ENT
+    int  21H
 SCAN_CHOICE:
-		MOV  AH, 8
-		INT  21H
-		
-		CMP  AL, '0'
-		JB   SCAN_CHOICE
-		CMP  AL, '9'
-		JA   SCAN_CHOICE
-		
-		MOV  BL, AL
-		XOR  BH, BH
-		
-		MOV  AH, 2
-		MOV  DL, AL
-		INT  21H
-		
-		MOV  AH, 9
-		MOV  DX, OFFSET NLINE
-		INT  21H
-		
+    mov  AH, 08h ;CHARACTER INPUT WITHOUT ECHO, CHAR IN AL
+    int  21H
+
+    cmp  AL, '0'
+    jb   SCAN_CHOICE
+    cmp  AL, '9'
+    ja   SCAN_CHOICE
+
+    mov  BL, AL
+    xor  BH, BH
+
+    mov  AH, 2
+    mov  DL, AL
+    int  21H
+
+    mov  AH, 9
+    mov  DX, offset NLINE
+    int  21H
 PROCESS:
-		SUB  BX, '0'
-		
-		CMP  BX, 8
-		JAE  EXIT
-		
-		CMP  BX, 0
-		JE   PMENU
-		
-		CMP  BX, 1
-		JE   INPUT_NUMBER
-		
-		SUB  BX, 2
-		SHL  BX, 1
-		
-		PUSH X
-		PUSH SI
-		CALL F[BX]
-		
-		JMP  PRINT_ENT
-		
+    sub  BX, '0'
+        
+    cmp  BX, 8
+    jae  EXIT
+        
+    cmp  BX, 0
+    je   PMENU
+
+    cmp  BX, 1
+    je   INPUT_NUMBER
+
+    sub  BX, 2
+    SHL  BX, 1
+
+    push X
+    push SI
+    call F[BX]
+    
+    jmp  PRINT_ENT
+
 INPUT_NUMBER:
-		CALL SCAN
-		
-		MOV  X, AX
-		MOV  SI, DX
-		
-		JMP  PRINT_ENT
-		
+    call SCAN
+
+    mov  X, AX
+    mov  SI, DX
+
+    jmp  PRINT_ENT
+
 EXIT:
-		MOV  AH, 4CH
-		XOR  AL, AL
-		INT  21H
-		
-CSEG	ENDS
-END BEGIN
+    mov  AH, 4CH
+    xor  AL, AL
+    int  21H
+
+cseg ends
+end BEGIN
